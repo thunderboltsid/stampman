bf36e307b13148d40e978ebb32151a3ea0e32cf9
Siddharth Shukla <siddharthshukla@outlook.com>
2016-11-28 22:42:39 +0100

    Add unit test for testing flask endpoint
    Test GET on the `/` endpoint

--- stampman/tests/test_api.py
+++ stampman/tests/test_api.py
@@ -1,6 +1,36 @@
 import unittest
-from stampman.services import pool
+import json
+
+import requests
+from stampman import main
 
 
-class TestAPIEndpoint(unittest.TestCase):
-    pass
+class TestAPIRoot(unittest.TestCase):
+    def setUp(self):
+        self._port = "8000"
+        self._path = "http://0.0.0.0"
+        main.app.config['TESTING'] = True
+        self._app = main.app.test_client()
+
+    def testGetJson(self):
+        response = self._app.get("/")
+        expected_response = [
+            {
+                "services": [
+                    {
+                        "name": "mailgun",
+                        "priority": 2
+                    },
+                    {
+                        "name": "sendgrid",
+                        "priority": 1
+                    }
+                ],
+                "url": "http://localhost/mail.sshukla.de",
+                "domain": "mail.sshukla.de"
+            }
+        ]
+        response_dict = json.loads(str(response.data, 'utf-8'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "application/json")
+        self.assertEqual(response_dict, expected_response)
